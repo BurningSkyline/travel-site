9e4c2b4433c94cb1df663a45de871b9c4365ddb7
BurningSkyline <xdeicide161x@gmail.com>
2018-08-12 19:54:53 +0300

    figured out?

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,22 +2,20 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 	browserSync.init({
 		notify: false,
 		server: {
 			baseDir: "app"
-		}
+			}
+		});
+
+		gulp.watch('./app/index.html').on('change', browserSync.reload)
 	});
 
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-
-	watch('./app/assets/styles/**/*.css', function(){
+	gulp.watch('./app/assets/styles/**/*.css', function(){
 		gulp.start('cssInject');
 	});
-});
 
 gulp.task('cssInject', ['styles'], function(){
 		return gulp.src('./app/temp/styles/styles.css')
