e29a17b27d17885109f0dbd345170cef22bc115f
BurningSkyline <xdeicide161x@gmail.com>
2018-08-12 20:17:32 +0300

    dasd

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,20 +2,22 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function() {
+gulp.task('watch', function(){
 	browserSync.init({
 		notify: false,
 		server: {
 			baseDir: "app"
-			}
-		});
-
-		gulp.watch('./app/index.html').on('change', browserSync.reload)
+		}
 	});
 
-	gulp.watch('./app/assets/styles/**/*.css', function(){
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
 		gulp.start('cssInject');
 	});
+});
 
 gulp.task('cssInject', ['styles'], function(){
 		return gulp.src('./app/temp/styles/styles.css')
